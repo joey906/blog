d6d084f9ce5c19f2ff8bd5d7c669b7533673e554
joey906 <jtf1owataeba2ymi@gmail.com>
2021-06-03 20:36:25 +0900

    AddherokuDB

--- JoeyBlog/config/app.php
+++ JoeyBlog/config/app.php
@@ -263,7 +263,7 @@ return [
             'className' => Connection::class,
             'driver' => Mysql::class,
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => 'us-cdbr-east-04.cleardb.com',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
@@ -273,9 +273,9 @@ return [
             /*
              * It is recommended to set these options through your environment or app_local.php
              */
-            //'username' => 'my_app',
-            //'password' => 'secret',
-            //'database' => 'my_app',
+            'username' => 'b0f8f6e66240a0',
+            'password' => '98952345',
+            'database' => 'heroku_6e5f75edadf464a',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
